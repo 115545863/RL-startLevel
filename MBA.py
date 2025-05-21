fa7707b5d6bf88d809aeddb5c9bb7f90758573d2
yanan <15963381971@163.com>
2025-05-21 11:31:10 +0800

    finish MBA

--- MBA.py
+++ MBA.py
@@ -90,6 +90,72 @@ class EGreedy(Solver):
         self.evaluation[k] += 1./(self.count[k]+1)* (r-self.evaluation[k])
             
         return k
+    
+# 方法二：随时间衰减的e贪心算法
+class EGreedy_time(Solver):
+    def __init__(self,bb,e = 0.01, init_pro = 1.0):
+        super(EGreedy_time,self).__init__(bb)
+        self.total_counts = 0
+
+        # 记录拉杆权重
+        self.evaluation = np.array([init_pro]*self.bb.k)
+        # # 记录e值
+        # self.e = e
+    def run_one_step(self):
+        self.total_counts+=1
+        # 判断概率
+        if np.random.random()<(1/self.total_counts):
+            k = np.random.randint(0,self.bb.k)
+        else:
+            k = np.argmax(self.evaluation)
+        # 更新权重值
+        r = self.bb.get_score(k)
+        self.evaluation[k] += 1./(self.count[k]+1)* (r-self.evaluation[k])
+            
+        return k
+    
+'''
+上置信界算法：A拉杆拉过很多次，B拉杆只拉过一次，该算法认为被拉过次数越少，这个拉杆越值得被探索，因此也引入了霍夫丁不等式中的不确定值的变量，随着次数增加，这个值降低
+霍夫丁不等式：有多个随机变量，每个随机变量的期望=经验期望（总值除以次数）＋不确定值
+'''
+
+class UCB(Solver):
+    def __init__(self, bb, coef, init_prob = 1.0):
+        super(UCB, self).__init__(bb)
+        # 总次数
+        self.total_count = 0
+        self.estimates = np.array([init_prob] * self.bb.k)
+        self.coef = coef
+    def run_one_step(self):
+        self.total_count+=1
+        ucb = self.estimates + self.coef * np.sqrt(np.log(self.total_count)/(2*(self.count+1)))
+        k = np.argmax(ucb)
+        r = self.bb.get_score(k)
+        self.estimates[k] += 1. / (self.count[k]+1) * (r-self.estimates[k])
+        return k
+
+
+'''
+汤普森采样：假定每根拉杆有一个概率，然后每次迭代根据实际拉杆的结果情况调整概率偏移，使其逐渐倾向于正确的概率分布，比如左边正确的多，右边少，那么就会倾向于左边
+
+'''
+
+class TS(Solver):
+    def __init__(self, bb):
+        super(TS,self).__init__(bb)
+        self.a = np.ones(self.bb.k)
+        self.b = np.ones(self.bb.k)
+    def run_one_step(self):
+        samples = np.random.beta(self.a, self.b)
+        k = np.argmax(samples)
+        r = self.bb.get_score(k)
+
+        self.a[k]+=r
+        self.b[k]+=(1-r)
+        return k
+
+
+
 # 绘图函数
 def plot_results(solvers, solver_names):
     """生成累积懊悔随时间变化的图像。输入solvers是一个列表,列表中的每个元素是一种特定的策略。
@@ -102,16 +168,53 @@ def plot_results(solvers, solver_names):
     plt.title('%d-armed bandit' % solvers[0].bb.k)
     plt.legend()
     plt.show()
-# test
-np.random.seed(1)
-# 初始化老虎机
-k = 20
-bb = BernoulliBandit(k)
-e = 0.2
-egreedy = EGreedy(bb,e)
-egreedy.run(500)
-print('epsilon-贪婪算法的累积懊悔为：', egreedy.regrets)
-plot_results([egreedy], ["EGreedy"])
 
+def test(name):
+    if name == "egreedy":
+        # test
+        np.random.seed(1)
+        # 初始化老虎机
+        k = 20
+        bb = BernoulliBandit(k)
+        e = 0.2
+        egreedy = EGreedy(bb,e)
+        egreedy.run(500)
+        print('epsilon-贪婪算法的累积懊悔为：', egreedy.regret_val)
+        plot_results([egreedy], ["EGreedy"])
+    elif name == "egreedy-time":
+        # test
+        np.random.seed(1)
+        # 初始化老虎机
+        k = 20
+        bb = BernoulliBandit(k)
+        e = 0.2
+        egreedy = EGreedy_time(bb,e)
+        egreedy.run(500)
+        print('衰减epsilon-贪婪算法的累积懊悔为：', egreedy.regret_val)
+        plot_results([egreedy], ["EGreedy_time"])
+    elif name == "UCB":
+        np.random.seed(1)
+        # 初始化老虎机
+        k = 20
+        bb = BernoulliBandit(k)
+        # 初始化参数
+        coef = 1
+        ucb_test = UCB(bb, coef)
+        ucb_test.run(500)
+        print('上置信界算法的累积懊悔为：', ucb_test.regret_val)
+        plot_results([ucb_test], ["UCB"])
+    elif name=="ts":
+        np.random.seed(1)
+        # 初始化老虎机
+        k = 20
+        bb = BernoulliBandit(k)
+        # 初始化参数
+        coef = 1
+        ts_test = TS(bb)
+        ts_test.run(500)
+        print('汤普森的累积懊悔为：', ts_test.regret_val)
+        plot_results([ts_test], ["TS"])
+    else:
+        print("没有这个方法")
 
-
+test("ts")
